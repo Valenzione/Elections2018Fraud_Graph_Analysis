ecdfa347c80d4ffbb65fb81059c1ad94333c74af
unknown <b.maksudov@innopolis.ru>
2018-04-19 22:21:22 +0300

    Added heatmap

--- classic_aproach.py
+++ classic_aproach.py
@@ -14,17 +14,27 @@ lost = 'Число утраченных избирательных бюллет�
 def z_score(uik_data):
     subregions = uik_data.groupby('subregion')
     uik_data['z_vw'] = (uik_data[putin] - subregions.transform('mean')[putin])/subregions.transform('std')[putin]
-    uik_data['z_t'] =  (uik_data[putin] - subregions.transform('mean')[putin])/subregions.transform('std')[putin]
+    uik_data['turnout'] = turnout(uik_data)
+    uik_data['z_t'] =  (turnout(uik_data)-uik_data.groupby('subregion')['turnout'].transform('mean'))/uik_data.groupby('subregion')['turnout'].transform('std')
     
 def turnout(uik_data):
     turnout = (uik_data[invalid] + uik_data[lost]+uik_data[valid])/uik_data[total_voters]
     return turnout
-    
-def turnout_subregions(subregions,transform):
-    turnout_series = subregions.apply(lambda x: x.assign(turnout = (x[invalid] + x[lost]+x[valid])/x[total_voters])).reset_index('subregion', drop = True)
-    return turnout_series
 
-    
+def draw_heatmap(uik_data):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    uik_data['turnout'] = uik_data['turnout']/100
+    x = uik_data['z_t'].values
+    y = uik_data[putin].values
+    plt.hist2d(x, y, bins=60,normed=False,cmap='plasma')
+    # Plot heatmap
+    plt.title('Pythonspot.com heatmap example')
+    plt.hlines(0,-1,2)
+    plt.vlines(0,-1,2)
+    plt.ylabel('z_vw')
+    plt.xlabel('z_t')
+    plt.show()    
     
 
 import pandas as pd
